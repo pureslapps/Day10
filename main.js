e1f5660b57a7165a6b97b2cfeb1e922a749c254e
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-06 10:50:34 -0400

    created examples of Document Selectors/Methods

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('Hello World')
+
+ function sayHello(){
+   document.write('Hello User')
+ }
+
+ sayHello()
+
+
+let x1 = document.getElementByClassName("header1")[0]
+console.log(x1)
+
+let x2 = document.GetElementsByTagName('h1')[0]
+console.log(x2)
+
+let x3 = documentGetElementById('header1')
+console.log(x3)
+
+let x4 = document.querySelector("#header1.header1")
+console.log(x4)
